279aa1d32dee60d8aacee91b8628e438d1b254c9
Nimrod Izraely <nimrodizr@gmail.com>
2018-11-18 15:14:36 +0200

    first stage

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const request = require('request-promise');
 const apiKey = process.env.SHOPIFY_API_KEY;
 const apiSecret = process.env.SHOPIFY_API_SECRET;
 const scopes = 'read_products';
-const forwardingAddress = "https://dcc6c8d5.ngrok.io"; // Replace this with your HTTPS Forwarding address
+const forwardingAddress = "https://e67d08dc.ngrok.io"; // Replace this with your HTTPS Forwarding address
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
