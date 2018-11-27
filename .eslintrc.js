97ee4befc4ddbc51bfc55816050d1c482e996c11
Nimrod Izraely <nimrodizr@gmail.com>
2018-11-27 12:38:07 +0200

    enable eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,11 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "node": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "object-curly-spacing": [
-            "error",
-            "always"
-        ],
-        "newline-per-chained-call": [
-            "error", 
-            { "ignoreChainWithDepth": 2 }
-        ],
-        "no-console": [
-            "warn",
-            "always"
-        ]
-    }
-};
+ "extends": "airbnb",
+ "env": {
+ "node": true,
+ "es6": true,
+ "browser": true
+ },
+ "rules": {
+ // your custom rules here
+ }
+}
