5bbbecdbc431997a40b1ed79185f86cc95da1d74
Nimrod Izraely <nimrodizr@gmail.com>
2018-12-05 18:41:12 +0200

    update keys & url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  appAddress: 'https://ad68d9ca.ngrok.io',
-  apiKey: 'fff0ba0b25de07b9c20caa2b37111b87',
-  apiSecret: 'fd7f77600db4d5e8b20e4f341a4d71c5',
-  scopes: 'read_products,write_script_tags'
+  appAddress: 'https://nimrod-chatbot.localtunnel.me',
+  apiKey: '1147408bd932ee445da9cf6d46d11cec',
+  apiSecret: '3d7bf0fae44459a85480462bbe07bd6d',
+  scopes: 'read_products,write_script_tags',
 };
